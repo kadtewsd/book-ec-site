5d544c77c14e40fe499a8986400164efa2070a67
kasakaid <kasakaid@outlook.com>
2018-08-04 01:45:10 +0900

    now, you can add or remove item correctly

--- src/reducer/handler/ChangeBookReducerType.ts
+++ src/reducer/handler/ChangeBookReducerType.ts
@@ -14,34 +14,47 @@ class ChangeBookReducerType implements IHandleBookReducer {
             return this.removeTheItem(state);
         }
         const results = state.cart.filter(book => book.isbn === this.asin);
-        const func = this.quantity < results.length ? (index: number, array: Book[]) => array.splice(index, 1) :
-            (index: number, array: Book[]) => array.push(array[index]);
-        const cart = this.addOrRemove(results, func);
+        const cart = this.addOrRemove(results);
         // filter の戻り値は配列なので book は配列として生成されている。そのため、スプレッドを book にも使う。
-        return { cart: [... state.cart.filter((book) => cart.find((book1) => book.isbn !== book1.isbn)), ...cart] }
+        return { cart: [...state.cart.filter((book) => cart.find((book1) => book.isbn !== book1.isbn)), ...cart] }
     }
 
     private removeTheItem(state: ICartState) {
-            const books: Book[] = [];
-            state.cart.forEach((book, index, array) => {
-                if (book.isbn !== this.asin) {
-                   books.push(book) ;
-                }
-            })
-            return {cart: books}
+        const books: Book[] = [];
+        state.cart.forEach((book, index, array) => {
+            if (book.isbn !== this.asin) {
+                books.push(book);
+            }
+        })
+        return { cart: books }
     }
 
-    private addOrRemove(results: Book[], func: (index: number, array: Book[]) => any): Book[] {
+    private addOrRemove(results: Book[]): Book[] {
         if (this.quantity === results.length) {
             return results;
         }
-        const loopCount = this.quantity < results.length ?
-            this.quantity - results.length : this.quantity - results.length;
+        if (this.quantity < results.length) {
+            this.remove(results);
+        } else {
+            this.add(results);
+        }
+        return results
+    }
 
+    private add(results: Book[]) {
+        const loopCount = this.quantity - results.length;
+        const func = (index: number, array: Book[]) => array.push(array[index]);
         for (let i = 0; i < loopCount; i++) {
             func(i, results);
         }
-        return results
+    }
+
+    private remove(results: Book[]) {
+        const loopCount = results.length - this.quantity
+        const func = (index: number, array: Book[]) => array.splice(index, 1);
+        for (let i = loopCount; 0 < i; i--) {
+            func(i, results);
+        }
     }
 }
 
