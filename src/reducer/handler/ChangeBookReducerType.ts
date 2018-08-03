2266a40eca5f4b4207ba8c075833f7c03e870f52
kasakaid <kasakaid@outlook.com>
2018-08-04 00:34:23 +0900

    now, you can remove item by setting 0

--- src/reducer/handler/ChangeBookReducerType.ts
+++ src/reducer/handler/ChangeBookReducerType.ts
@@ -10,12 +10,25 @@ class ChangeBookReducerType implements IHandleBookReducer {
         return BookReducerHanlerType.CHANGE.toString();
     }
     public handle(state: ICartState, action: IBookAction): { cart: Book[] } {
+        if (this.quantity === 0) {
+            return this.removeTheItem(state);
+        }
         const results = state.cart.filter(book => book.isbn === this.asin);
         const func = this.quantity < results.length ? (index: number, array: Book[]) => array.splice(index, 1) :
             (index: number, array: Book[]) => array.push(array[index]);
         const cart = this.addOrRemove(results, func);
         // filter の戻り値は配列なので book は配列として生成されている。そのため、スプレッドを book にも使う。
         return { cart: [... state.cart.filter((book) => cart.find((book1) => book.isbn !== book1.isbn)), ...cart] }
+    }
+
+    private removeTheItem(state: ICartState) {
+            const books: Book[] = [];
+            state.cart.forEach((book, index, array) => {
+                if (book.isbn !== this.asin) {
+                   books.push(book) ;
+                }
+            })
+            return {cart: books}
     }
 
     private addOrRemove(results: Book[], func: (index: number, array: Book[]) => any): Book[] {
